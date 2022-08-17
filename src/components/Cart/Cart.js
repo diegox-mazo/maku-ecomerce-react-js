9e3acfd374315402d0749803233196caba1f865c
Diego Mazo <diegomazofl@gmail.com>
2022-08-17 17:51:39 -0500

    Maquetado visual Cart View

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -10,16 +10,38 @@ function Cart(){
     return(
         
         <div>
-            <h1>Cart</h1>
-            
-            {
-                cart && cart.length && cart.map((product)=>{
-                    return  <div key={product.id}>ID: {product.id} - Nombre: {product.title} - Cantidad: {product.quantity}</div>
-                })
-            }
-            
+            <h1 className="m-4">Cart</h1>
+            <div className="container text-center mt-5">
+                <div className="row">
+                    <div className="col-md-9 me-2">
+                    {
+                        cart && cart.length && cart.map((product)=>{
+                            return (
+
+                                <div key={product.id} className="container text-center py-4 border border-2 rounded mb-3">
+                                    <div className="row align-items-center">
+                                        <div className="col-2"><img className='w-50' src={product.pictureUrl} alt={product.title}></img></div>
+                                        <div className="col-1">{product.id}</div>
+                                        <div className="col">{product.title}</div>
+                                        <div className="col-1">{product.quantity}</div>
+                                        <div className="col-3"><button className='btn btn-danger btn-sm'>Eliminar</button></div>
+                                    </div>
+                                </div>                                                   
+                            );                    
+                        })
+                    }
+                    </div>
+
+                    <div className="col-md-2 border border-2 rounded p-3 mb-3">
+                        <div className='mb-2'>Level 1: .col-sm-3</div>
+                        <button className='btn btn-primary'>Finalizar</button>
+                    </div>                    
+                </div>
+            </div>            
         </div>
     );
 }
 
 export default Cart;
+
+/* <div >ID: {product.id} - Nombre: {product.title} - Cantidad: {product.quantity}</div> */
--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -8,15 +8,13 @@ function CartWidget(prop){
 
 
     return(
-        <div className='shopping-cart ms-5 pe-3'>
-            <a href="#link" className="d-flex link-primary">
-                <img
-                    src={cartLogo}
-                    className="d-inline-block align-top cart-logo"
-                    alt="Shopping Cart"
-                />
-                <label className='align-self-center'>{prop.counter}</label>
-            </a>
+        <div className='shopping-cart ms-5 pe-3 d-flex'>
+            <img
+                src={cartLogo}
+                className="d-inline-block align-top cart-logo"
+                alt="Shopping Cart"
+            />
+            <label className='align-self-center'>{prop.counter}</label>
         </div>
     );
 }
